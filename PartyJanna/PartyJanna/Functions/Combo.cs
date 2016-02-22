1da539fbb0cae514604d2eda24832e0e4e522ec1
houzeparty <luccavinicius.gaming3673@gmail.com>
2016-02-21 21:26:58 -0300

    pu

--- PartyJanna/PartyJanna/Functions/Combo.cs
+++ PartyJanna/PartyJanna/Functions/Combo.cs
@@ -1,23 +1,49 @@
 ﻿using EloBuddy.SDK;
 using EloBuddy;
+using EloBuddy.SDK.Menu.Values;
+using System.Collections.Generic;
 
 namespace PartyJanna.Functions
 {
     public static class Combo
     {
+        public static List<string> PriorityOrder;
+
+        private static int HighestPriority = 0;
+
         public static void Execute()
         {
             Startup.CurrentFunction = "Combo";
 
-            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            PriorityOrder = new List<string>();
+
+            foreach (Slider PrioritySlider in Config.Protect.PrioritySliderList)
             {
-                foreach (var Element in Passive.PriorityOrder)
+                if (PrioritySlider.CurrentValue >= HighestPriority)
+                {
+                    HighestPriority = PrioritySlider.CurrentValue;
+                    PriorityOrder.Insert(0, PrioritySlider.VisibleName);
+                }
+                else
+                {
+                    PriorityOrder.Add(PrioritySlider.VisibleName);
+                }
+            }
+
+                /*foreach (Slider PrioritySlider in Config.Protect.PrioritySliderList)
+                {
+                    PriorityOrder.Add(PrioritySlider.VisibleName.Insert(0, PrioritySlider.CurrentValue.ToString()));
+                }
+
+                PriorityOrder.Sort();
+                PriorityOrder.Reverse();*/
+
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                foreach (var Element in PriorityOrder)
                 {
                     Chat.Print(Element);
                 }
-
-                Chat.Print("\n");
-                Passive.PriorityOrder.Clear();
             }
         }
     }
--- PartyJanna/PartyJanna/Functions/Passive.cs
+++ PartyJanna/PartyJanna/Functions/Passive.cs
@@ -8,21 +8,15 @@ namespace PartyJanna.Functions
 {
     public static class Passive
     {
-        public static List<string> PriorityOrder = new List<string>();
+        //public static List<string> PriorityOrder = new List<string>();
 
-        private static int HighestPriority = 0;
+        //private static int HighestPriority = 0;
+
+
 
         public static void Execute()
         {
             Startup.CurrentFunction = "Protect";
-
-            foreach (Slider PrioritySlider in Config.Protect.PrioritySliderList)
-            {
-                PriorityOrder.Add(PrioritySlider.VisibleName.Insert(0, PrioritySlider.CurrentValue.ToString()));
-            }
-
-            PriorityOrder.Sort();
-            PriorityOrder.Reverse();
 
             /*foreach (AIHeroClient Enemy in EntityManager.Heroes.Enemies)
             {
