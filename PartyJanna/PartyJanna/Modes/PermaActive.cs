ddc43aed726d51ccaa4dc40751b47ed93cfb63a8
houzeparty <luccavinicius.gaming3673@gmail.com>
2016-02-26 18:16:31 -0300

    up

---
+++ PartyJanna/PartyJanna/Modes/PermaActive.cs
@@ -0,0 +1,22 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+using SharpDX;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PartyJanna.Modes
+{
+    public class PermaActive : ModeBase
+    {
+        public override bool ShouldBeExecuted()
+        {
+            return true;
+        }
+
+        public override void Execute()
+        {
+
+        }
+    }
+}
---
+++ PartyJanna/PartyJanna/Program.cs
@@ -0,0 +1,46 @@
+﻿using EloBuddy;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+using System;
+using PartyJanna.Modes;
+
+namespace PartyJanna
+{
+    public static class Program
+    {
+        public const string ChampName = "Janna";
+
+        public static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+
+        private static void OnLoadingComplete(EventArgs args)
+        {
+            if (Player.Instance.ChampionName != ChampName)
+            {
+                return;
+            }
+
+            Config.Initialize();
+            SpellManager.Initialize();
+            ModeManager.Initialize();
+            Events.Initialize();
+
+            Drawing.OnDraw += OnDraw;
+        }
+
+        private static void OnDraw(EventArgs args)
+        {
+            Circle.Draw(Color.White, SpellManager.Q.Range, Player.Instance.Position);
+            Circle.Draw(Color.White, SpellManager.W.Range, Player.Instance.Position);
+            Circle.Draw(Color.White, SpellManager.E.Range, Player.Instance.Position);
+            Circle.Draw(Color.White, SpellManager.R.Range, Player.Instance.Position);
+
+            Circle.Draw(Color.Red, 100, Player.Instance.Position);
+            Circle.Draw(Color.Red, 200, Player.Instance.Position);
+            Circle.Draw(Color.Red, 300, Player.Instance.Position);
+        }
+    }
+}
