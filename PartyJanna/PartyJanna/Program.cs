0b06af1eaa264d5ba3a180b5ec1bf2d32b213188
houzeparty <luccavinicius.gaming3673@gmail.com>
2016-02-27 13:06:42 -0300

    up

--- PartyJanna/PartyJanna/Program.cs
+++ PartyJanna/PartyJanna/Program.cs
@@ -3,7 +3,6 @@ using EloBuddy.SDK.Events;
 using EloBuddy.SDK.Rendering;
 using SharpDX;
 using System;
-using Settings = PartyJanna.Config.Modes.Draw;
 
 namespace PartyJanna
 {
@@ -33,22 +32,22 @@ namespace PartyJanna
 
         private static void OnDraw(EventArgs args)
         {
-            if (Settings.DrawQ)
+            if (PartyJanna.Config.Modes.Draw.DrawQ)
             {
                 Circle.Draw(Color.White, SpellManager.Q.Range, Player.Instance.Position);
             }
 
-            if (Settings.DrawW)
+            if (PartyJanna.Config.Modes.Draw.DrawW)
             {
                 Circle.Draw(Color.White, SpellManager.W.Range, Player.Instance.Position);
             }
 
-            if (Settings.DrawE)
+            if (PartyJanna.Config.Modes.Draw.DrawE)
             {
                 Circle.Draw(Color.White, SpellManager.E.Range, Player.Instance.Position);
             }
 
-            if (Settings.DrawR)
+            if (PartyJanna.Config.Modes.Draw.DrawR)
             {
                 Circle.Draw(Color.White, SpellManager.R.Range, Player.Instance.Position);
             }
