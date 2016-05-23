03d7cbcad64ed136afb759e5214ba38a8c39ba7e
houzeparty <luccavinicius.gaming3673@gmail.com>
2016-05-23 18:21:39 -0300

    CSL Released!

--- CustomSkillLevel/CustomSkillLevel/Properties/AssemblyInfo.cs
+++ CustomSkillLevel/CustomSkillLevel/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
-[assembly: AssemblyTitle("CustomSkillLeveler")]
+[assembly: AssemblyTitle("CustomSkillLevel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CustomSkillLeveler")]
+[assembly: AssemblyProduct("CustomSkillLevel")]
 [assembly: AssemblyCopyright("Copyright © houzeparty 2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -16,4 +16,4 @@ using System.Security.Permissions;
 [assembly: Guid("3203502a-4253-4edd-9331-74a49c616ed2")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("6.10.0.0")]
