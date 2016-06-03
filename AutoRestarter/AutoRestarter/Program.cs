832e18f1538dd309902785d67ba595f0d875f4d4
houzeparty <luccavinicius.gaming3673@gmail.com>
2016-06-03 10:35:27 -0300

    up

--- AutoRestarter/AutoRestarter/Program.cs
+++ AutoRestarter/AutoRestarter/Program.cs
@@ -5,16 +5,23 @@ namespace AutoRestarter
 {
     class Program
     {
+        static int time;
+        static string botPath;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Time for each restart (sec): ");
+            time = Convert.ToInt32(Console.ReadLine());
+
             while (true)
             {
                 foreach (Process sysProcess in Process.GetProcesses())
                 {
-                    if (sysProcess.ProcessName.Contains("ezBot"))
+                    if (sysProcess.ProcessName.Contains("bot"))
                     {
+                        botPath = sysProcess.MainModule.FileName;
                         sysProcess.Kill();
-                        Console.WriteLine("[{0:hh:mm:ss}] closing ezBot..", DateTime.Now);
+                        Console.WriteLine("[{0:hh:mm:ss}] closing bot..", DateTime.Now);
                     }
                     else if (sysProcess.ProcessName.Contains("League of Legends"))
                     {
@@ -23,8 +30,8 @@ namespace AutoRestarter
                     }
                 }
 
-                Process.Start("ezBot.exe");
-                Console.WriteLine("[{0:hh:mm:ss}] initializing ezBot..", DateTime.Now);
+                Process.Start(botPath);
+                Console.WriteLine("[{0:hh:mm:ss}] initializing bot..", DateTime.Now);
 
                 System.Threading.Thread.Sleep(3600000);
             }
